11bd738105727935c2b20fd3527d85580bbc1865
Aphiwad Chhoeun <aphiwad.chhoeun@gmail.com>
2020-04-19 12:50:08 -0700

    adjust styling for filter tags

--- src/components/FilterTag.vue
+++ src/components/FilterTag.vue
@@ -14,7 +14,6 @@
   background-color: $LIGHT_CYAN_2;
   margin-right: 5px;
   margin-bottom: 10px;
-  padding: 4px;
   border-radius: 4px;
   font-size: $TEXT_SIZE - 2;
   cursor: pointer;
@@ -26,14 +25,28 @@
   &:hover {
     background-color: $PRIMARY_COLOR;
     color: $LIGHT_CYAN;
+
+    .icon-remove {
+      background-color: $VERY_DARK_CYAN;
+    }
+  }
+
+  &__text {
+    padding: 4px;
+    display:inline-block;
   }
 
   .icon-remove {
     background-image: url("../../images/icon-remove.svg");
+    background-color: $PRIMARY_COLOR;
     background-repeat: no-repeat;
+    background-position: center;
+    vertical-align: middle;
     display: inline-block;
     width: 15px;
     height: 15px;
+    border-radius: 0 4px 4px 0;
+    padding: 4px;
   }
 }
 </style>
